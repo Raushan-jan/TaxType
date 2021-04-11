16ce3630dd695e57458e46e94cb1f5c1c8a18e5f
Админ <rauchan.87@mail.ru>
2021-04-11 10:47:15 +0600

    Initial commit

---
+++ src/IncomeTaxType.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public class IncomeTaxType implements TaxType {
+
+    @Override
+    public BigDecimal calculateTaxFor(BigDecimal amount) {
+        return amount.multiply(new BigDecimal("0.13"));
+    }
+}
---
+++ src/TaxType.java
@@ -0,0 +1,6 @@
+import java.math.BigDecimal;
+
+public interface TaxType {
+
+    BigDecimal calculateTaxFor(BigDecimal amount);
+}
